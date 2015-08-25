5154eceda19c4a35f452a701802fa1415afbf5b9
FMCalisto <francisco_mcalisto@hotmail.com>
2015-08-25 11:21:00 +0100

    [CHANGE] Final HFP4

--- prototypes/HFP4/wordpresstest/wp-content/themes/accesspress-lite/index-one.php
+++ prototypes/HFP4/wordpresstest/wp-content/themes/accesspress-lite/index-one.php
@@ -536,7 +536,7 @@ wp_reset_query(); ?>
                 <?php endwhile; ?>
 				</div>
 			</div>
-			<a class="all-testimonial" href="<?php echo get_category_link( $testimonial_category ) ?>"><?php echo esc_html($accesspresslite_settings['view_all_text']); ?> <?php echo get_cat_name($testimonial_category); ?></a>
+			<a class="all-testimonial" href="<?php echo get_category_link( $testimonial_category ) ?>">
 	        
 	        
 	        <?php wp_reset_postdata(); 
